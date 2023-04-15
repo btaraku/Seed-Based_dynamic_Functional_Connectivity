d8981bc0186c031cb79d25425327d6c3de029658
btaraku <88472760+btaraku@users.noreply.github.com>
2023-04-14 17:56:15 -0700

    Update SBC_dFCv.py

--- SBC_dFCv.py
+++ SBC_dFCv.py
@@ -22,28 +22,38 @@ def SBC_dFCv_map(data_ts, seed_ts, w_size, w_step, wHamm):
 	# Data expected to be in time (rows) by brain vertex/voxel (cols)
 
 	# Znormalize data
+	# Must be done if hamming window is used
 	data_ts_znorm = scipy.stats.zscore(data_ts,axis=0)
 	seed_ts_znorm = scipy.stats.zscore(seed_ts,axis=0)
 
+	# Exit function if Seed timepoints do not match whole brain data timepoints
 	if seed_ts.size != data_ts.shape[0]:
 		print("seed and data timepoints do not match")
 		print(seed_ts.size)
 		print(data_ts.shape[0])
 		return None
 
-	# Sliding window parameters
+	# Sliding window parameters:
+	#
+	# number of timepoints in data
 	n_tps = seed_ts.size
+	# number of spatial data points in data (i.e. number of voxels or number of CIFTI greyordinates)
 	n_pts = data_ts.shape[1]
+	# number sliding windows to be computed
 	n_window = int((n_tps - w_size)/w_step + 1);
+	# window starting timepoint
 	start = 0
+	# window ending timepoint
 	end = w_size
 	
+	# Create a window using a window function
 	if wHamm:
 		print("Using hamming window")
 		window = np.hamming(w_size)
 	else:
 		print("Using rectangular window")
 		window = np.ones(w_size)
+	# Create matrix of windows for windowing of all data points
 	data_window = np.tile(window, (n_pts,1)).T
 	seed_window = window
 
@@ -53,21 +63,26 @@ def SBC_dFCv_map(data_ts, seed_ts, w_size, w_step, wHamm):
 	for w in range(n_window):
 		
 		print("Calculating window "+str(w))
+		# Window whole brain data
 		w_data_ts = data_ts_znorm[start:end]
 		w_data_ts = np.multiply(w_data_ts, data_window) 	
 	
+		# Window seed data
 		w_seed_ts = seed_ts_znorm[start:end]
 		w_seed_ts = np.multiply(w_seed_ts, seed_window)
 
+		# Compute correlation between seed and whole brain windowed timeseries
 		w_corr = np_pearson_corr(w_seed_ts, w_data_ts)
 
 		# Fishers r to z transformation using inverse hyperbolic tangent function
 		w_zcorr = np.arctanh(w_corr)
 		
+		# Store result in sliding windows matrix
 		SW_data[w] = w_zcorr
 
-		start=start+1
-		end=end+1
+		# Increment window boundaries by window step size
+		start=start+w_step
+		end=end+w_step
 	
 	# Compute Standard Deviation across Windows
 	dFCv_zcorr = np.std(SW_data, axis=0, ddof=1)
@@ -90,7 +105,7 @@ def gen_SBC_dFCv(data_path, seed_path, out_file, w_size, w_step, wHamm, transpos
 	np.savetxt(out_file, dFCv_map)
 
 
-
+# Retrieve arguments
 data_path = sys.argv[1]
 seed_path = sys.argv[2]
 out_file = sys.argv[3]
